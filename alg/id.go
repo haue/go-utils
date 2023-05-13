bb0e929225682e7a6eaf702dba8d618a9fd799c0
haue <haue@live.cn>
2023-05-13 11:58:44 +0800

    .

--- alg/id.go
+++ alg/id.go
@@ -4,16 +4,22 @@ import (
 	"github.com/bwmarrin/snowflake"
 )
 
-// 生成id
-// StepBits 10位
-func IdGen() (int64, error) {
+var node *snowflake.Node
+
+func init() {
 	snowflake.NodeBits = 1
 	snowflake.Epoch = 1681987346000
 	snowflake.StepBits = 10
-	node, err := snowflake.NewNode(0)
+	n, err := snowflake.NewNode(0)
 	if err != nil {
-		return 0, err
+		panic(err)
 	}
+	node = n
+}
+
+// 生成id
+// StepBits 10位
+func Id() (int64, error) {
 	id := node.Generate()
 	return id.Int64(), nil
 }
